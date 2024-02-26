00abf5f66a7d4ae0c786978970f43138880e8d72
Egor Mishin <em@egormishin.com>
2024-02-26 17:31:28 +0100

    init: add test view

--- dist/app.js
+++ dist/app.js
@@ -1,14 +1,54 @@
-class  App {
-	routes= [
-		{path: "", view: null}
-	]
-	constructor() {
-		window.addEventListener('hashchange', this.route.bind(this));
-	}
+(function () {
+    class AbstractView {
+        constructor() {
+            this.app = document.getElementById('root'); // Create a root html element
+        }
+        setTitle(title) {
+            document.title = title;
+        }
+    // Render method
+        render() {
+            return;
+        }
+    // Destroy method
+        destroy() {
+            return;
+        }
+    }
 
-	route() {
-		const view = this.routes.find(r=> r.path === location.hash)
-	}
-}
+    class MainView extends AbstractView {
+        constructor() {
+            super();
+            this.setTitle('Book search');
+        }
+        render() {
+            const main = document.createElement('div');
+            main.innerHTML = 'Test';
+            this.app.innerHTML = '';
+            this.app.append(main);
 
-new App();
+        }
+    }
+
+    class  App {
+        routes= [
+            {path: "", view: MainView}
+        ]
+        constructor() {
+            window.addEventListener('hashchange', this.route.bind(this));
+            this.route();
+        }
+
+        route() {
+            if(this.currentView){
+                this.currentView.destroy();
+            }
+            const view = this.routes.find(r=> r.path === location.hash).view;
+            this.currentView = new view();
+            this.currentView.render();
+        }
+    }
+
+    new App();
+
+})();
