b9f507db5004a7a6e243eb0aad9b0dc8151d5f5d
Egor Mishin <em@egormishin.com>
2024-02-27 18:51:51 +0100

    feat: add subscribrion func lib on-change

--- src/views/main/main.js
+++ src/views/main/main.js
@@ -1,14 +1,31 @@
 import {AbstractView} from "../../common/view.js";
+import onChange from  "on-change";
 
 export class MainView extends AbstractView {
-    constructor() {
+    state= {
+        list: [],
+        searchQuery: undefined,
+        loading: false,
+        offset: 0
+
+    }
+    constructor(appState) {
         super();
+        this.appState = appState
+        this.appState = onChange(this.appState, this.appStateHook.bind(this))
         this.setTitle('Book search');
+    }
+
+    appStateHook(path) {
+       if(path === 'favorites') {
+           console.log(path)
+       }
     }
     render() {
         const main = document.createElement('div')
-        main.innerHTML = 'Test';
+        main.innerHTML = `Число книг:${this.appState.favorites.length}`;
         this.app.append(main);
+        this.appState.favorites.push('d')
 
     }
 }
